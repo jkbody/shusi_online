ffa4d8a6dd90ae8efae80908b6e602736cb432b1
jiangkun <jiangkun00007@outlook.com>
2019-05-02 01:08:02 +0800

    comments request is ok

--- server/controllers/commentlist.js
+++ server/controllers/commentlist.js
@@ -1,17 +1,27 @@
 const {mysql} = require('../qcloud')
 module.exports = async (ctx) => {
     const {bookId} = ctx.request.query
+    console.log(bookId)
     const data = await mysql('comments').select('comments.*', 'csessioninfo.user_info').join('csessioninfo', 'comments.open_id', 'csessioninfo.open_id').where('comments.book_id', bookId)
     console.log(data)
-//     // ctx.state.data = {
-//     //     books: books.map(v => { // 映射处理数组，展开数组遍历每一项
-//     //         const info = JSON.parse(v.user_info) // 将user_info转换成对象
-//     //         // 合并数组将原来数组里面的user_info 替换成只有一个nickName的对象
-//     //         return Object.assign({}, v, {
-//     //             user_info: {
-//     //                 nickName: info.nickName
-//     //             }
-//     //         })
-//     //     })
-//     // }
+    try {
+        const comments = data.map(value => {
+            const info = JSON.parse(value.user_info)
+            return Object.assign({}, value, {
+                user_info: {
+                    name: info.nickName,
+                    image: info.avatarUrl
+                }
+            })
+        })
+        ctx.state.data = {
+            code: 0,
+            data: comments
+        }
+    } catch (e) {
+        ctx.state.data = {
+            code: -1,
+            msg: '请求失败' + e.sqlMessage
+        }
+    }
 }
